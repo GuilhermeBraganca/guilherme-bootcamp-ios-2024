c2006a8693847c8b1163f26d6828bb3fa34ff3a5
Guilherme <guilherme.braganca2@gmail.com>
2024-06-27 19:30:14 -0300

    Atualizacao com POO

--- POO-Bootcamp-Guilherme/POO-Bootcamp-Guilherme/main.swift
+++ POO-Bootcamp-Guilherme/POO-Bootcamp-Guilherme/main.swift
@@ -7,5 +7,100 @@
 
 import Foundation
 
-print("Hello, World!")
+//MARK: - Classe -> Modelo de um OBJETO!
+// Atributo / Propriedades = caracteristicas de um objeto
+// Ações = função / métodos
+// CLASSE TRABALHA COM REFERENCE TYPE
 
+// Como criar uma classe?
+// Utilizamos a palavra reservada 'class' e na sequencia seu nome sendo, SEMPRE COM A PRIMEIRA LETRA MAIUSCULA.
+
+class Automovel {
+
+  let cor: String = "Vermelho"
+  let quantidadeDeEspelho: Int = 5
+  let marca: String = "Ferrari"
+  let tipoCombustivel: String = "Alcool"
+  let tipoMotor: String = "AP"
+  let potencia: Double = 2.0
+  let conversivel: Bool = true
+  var cavaloDePotencia: Int = 900
+
+  func ligarCarro() {
+    print("O carro da marca \(marca) está ligado")
+  }
+
+  func acenderFarois() {
+    print("As luzes estão ligadas")
+  }
+
+  func detalheCompletoAutomovel() -> String {
+    let texto: String = "O veiculo da marca: \(marca) tem o motor: \(tipoMotor) que utiliza o combustivel \(tipoCombustivel) e tem um total de \(cavaloDePotencia) cavalos de potencia"
+    return texto
+  }
+
+}
+
+
+// Criando objeto!
+// Um objeto só ganha VIDA, QUANDO GERAMOS A SUA INSTANCIA QUE É O ABRE E FECHA PARENTESES ()
+var ferrari: Automovel = Automovel()
+
+//print(ferrari.cavaloDePotencia)
+//print(ferrari.cor)
+//print(ferrari.conversivel)
+//print(ferrari.detalheCompletoAutomovel())
+
+// Para alterar uma propriedade, você precisa chamar o objeto e atribuir um novo valor a propriedade
+ferrari.cavaloDePotencia = 1200
+print(ferrari.cavaloDePotencia)
+
+var uno: Automovel = Automovel()
+uno.cavaloDePotencia = 1500
+print(uno.cavaloDePotencia)
+
+
+//MARK: - Construtores
+
+class Pessoa {
+  var nome: String
+  var idade: Int
+  var peso: Double
+  var altura: Double
+
+  // Método Construtor -> Ele é quem seta/informa os valores iniciais!!!
+  init(nome: String, idade: Int, peso: Double, altura: Double) {
+    self.nome = nome
+    self.idade = idade
+    self.peso = peso
+    self.altura = altura
+  }
+
+}
+
+// Neste exemplo, estamos gerando a instancia de uma forma diferente -> a instancia é gerada QUANDO preenchido todas as propriedades
+// do metodo construtor
+var caio: Pessoa = Pessoa(nome: "Caio", idade: 22, peso: 80, altura: 1.82)
+print(caio.idade)
+print(caio.peso)
+
+var pedro: Pessoa = Pessoa(nome: "Pedro", idade: 45, peso: 92, altura: 1.77)
+print(pedro.idade)
+print(pedro.peso)
+
+// Se caso NÃO TEM VALOR INICIAL, VOCÊ PRECISA PASSAR ELE NO INIT!!!!!!
+class Celular {
+  var valorPiCelular: Double = 3.14
+  var tamanho: Double = 1.432432
+  var cor: String
+  var preco: Double
+  var modelo: String
+
+  init(modelo: String, preco: Double, cor: String) {
+    self.modelo = modelo
+    self.preco = preco
+    self.cor = cor
+  }
+}
+
+var iphone: Celular = Celular(modelo: "Iphone 15", preco: 8000, cor: "Preto")
